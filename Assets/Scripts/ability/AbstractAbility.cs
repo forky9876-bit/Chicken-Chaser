295147db8030f41dd05ae54edcda9f22be6ccacc
whatever you want for your username, it doesn't really matter <itdoeshavetohave@something.something>
2025-08-12 15:34:49 -0400

    gutgutgudtjrfuymhfjhmgnfbn

--- Assets/Scripts/ability/AbstractAbility.cs
+++ Assets/Scripts/ability/AbstractAbility.cs
@@ -1,8 +1,10 @@
 using System.Collections;
+using System.Threading;
 using UnityEngine;
 
 public abstract class AbstractAbility : MonoBehaviour
 {
+    #region words
     [SerializeField] Sprite iconicIcon;
     [SerializeField] bool canBeHeld;
     [SerializeField] float cooldown;
@@ -11,6 +13,8 @@ public abstract class AbstractAbility : MonoBehaviour
     private bool _isReady = true;
     private bool _isBeingHeld;
     private float _currentCooldownTime;
+    #endregion
+    #region this is the code
     void Start()
     {
         owner = GetComponentInParent<Checkin>();
@@ -18,11 +22,23 @@ public abstract class AbstractAbility : MonoBehaviour
     }
     public void StartAbility()
     {
-
+        _isBeingHeld = true;
+        if (_isReady)
+        {
+            StartCoroutine(ChatCooldownever());
+        }
+        if (IsBoolAnimation())
+        {
+            whatAreWeGoingToCallIt.SetBool(AbilityBoolID(), true);
+        }
     }
     public void StopAbility()
     {
-
+        _isBeingHeld = false;
+        if (IsBoolAnimation())
+        {
+            whatAreWeGoingToCallIt.SetBool(AbilityBoolID(), false);
+        }
     }
     public float GetCooldownPercentage()
     {
@@ -30,7 +46,29 @@ public abstract class AbstractAbility : MonoBehaviour
     }
     private IEnumerator ChatCooldownever()
     {
-        yield return null;
+        do
+        {
+            yield return new WaitUntil(CanActivate);
+            if (!_isBeingHeld)
+            {
+                yield break;
+            }
+            Activate();
+            if (IsTriggerAnimation())
+            {
+                whatAreWeGoingToCallIt.SetTrigger(AbilityTriggerID());
+            }
+            _currentCooldownTime = 0;
+            _isReady = false;
+            while (_currentCooldownTime < cooldown)
+            {
+                _currentCooldownTime += Time.deltaTime;
+                yield return null;
+            }
+            _currentCooldownTime = cooldown;
+            _isReady = true;
+        } while (_isBeingHeld && canBeHeld);
+        StopAbility();
     }
     public Sprite GetIconicIcon()
     {
@@ -38,10 +76,35 @@ public abstract class AbstractAbility : MonoBehaviour
     }
     private bool IsTriggerAnimation()
     {
-        return false;
+
+        return AbilityTriggerID() != 0;
     }
-    private bool IsBooleanAnimation()
+    private bool IsBoolAnimation()
     {
-        return false;
+        return AbilityBoolID() != 0;
     }
+    #endregion
+    #region inheritance
+    //a few more methods
+    public virtual bool CanActivate()
+    {
+        return _isReady;
+    }
+    protected abstract void Activate();
+    public virtual void ForceCancelAbility()
+    {
+        _currentCooldownTime = cooldown;
+        _isReady = true;
+        StopAllCoroutines();
+        StopAbility();
+    }
+    protected virtual int AbilityBoolID()
+    {
+        return 0;
+    }
+    protected virtual int AbilityTriggerID()
+    {
+        return 0;
+    }
+    #endregion
 }
